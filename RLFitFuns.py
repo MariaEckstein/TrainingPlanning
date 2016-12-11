a3d3a398bf063962e85d04929120c5a53cd13e69
Maria Eckstein <maerry.eckstein@gmail.com>
2016-12-11 03:47:27 -0800

    simulate or fit using the same function

--- RLFitFuns.py
+++ RLFitFuns.py
@@ -47,32 +47,118 @@ def select_stage2_fractals(frac1, common=0.8):
     return fractals2
 
 
-def simulate_task(n_agents, fractal_rewards=[0, .3, .7, 1], n_trials=100, common=0.8):
+# def simulate_task(n_agents, fractal_rewards=[0, .3, .7, 1], n_trials=100, common=0.8):
+#
+#     # Set up big DataFrame that will hold all the simulated agents' data
+#     colnames = ['AgentID', 'Q1_0', 'Q1_1', 'Q2_0', 'Q2_1', 'Q2_2', 'Q2_3', 'frac1', 'frac2', 'reward']
+#     agent_length = n_trials + 1
+#     Data = pd.DataFrame(columns=colnames, index=range(n_agents * agent_length))
+#
+#     # Loop through agents and generate data for each one
+#     for agent in range(n_agents):
+#
+#         # Initialize DataFrame for agent's behavior
+#         agent_rows = agent * agent_length + np.array(range(agent_length))
+#         Data.set_value(agent_rows, 'AgentID', agent)   # initial 1st-stage fractals
+#         # Agent = pd.DataFrame(columns=colnames, index=range(n_trials + 1))
+#         # Agent['AgentID'] = agent   # initial 1st-stage fractals
+#
+#         # Initial values for fractals
+#         Q1_cols = [''.join(['Q1_', str(i)]) for i in range(2)]
+#         Q2_cols = [''.join(['Q2_', str(i)]) for i in range(4)]
+#         Data.set_value(agent_rows[0], Q1_cols, .5 * np.ones(2))   # initial 1st-stage fractals
+#         Data.set_value(agent_rows[0], Q2_cols, .5 * np.ones(4))   # initial 2nd-stage fractals
+#         # Agent.set_value(0, 'Q1', .5 * np.ones(2))   # initial 1st-stage fractals
+#         # Agent.set_value(0, 'Q2', .5 * np.ones(4))   # initial 2nd-stage fractals
+#
+#         # Individual behavioral parameters
+#         par = np.random.rand(6)
+#         alpha1 = par[0] / 2
+#         alpha2 = par[1] / 2
+#         beta1 = par[2] * 10
+#         beta2 = par[3] * 10
+#         lambd = par[4] / 2 + 0.5
+#         w = par[5]
+#
+#         # Let agent play the game
+#         for t in range(n_trials):
+#
+#             # Get the right row in Data for this agent in this trial
+#             a_t = agent_rows[0] + t
+#
+#             # STAGE 1
+#             # Display fractals
+#             fractals1 = [0, 1]
+#
+#             # Agent's probability of choosing each fractal, gives their values
+#             prob_frac1 = softmax_Q2p(fractals1, Data.loc[a_t, Q1_cols], beta1)   # 1 / (1 + e ** (beta * (Qa - Qb))
+#
+#             # Agent picks one of the two fractals based on their probability
+#             frac1 = np.random.choice(fractals1, p=prob_frac1)
+#
+#             # STAGE 2
+#             # Display fractals
+#             fractals2 = select_stage2_fractals(frac1)   # frac1 == 0 => usually [0, 1]; frac1 == 1 => usually [2, 3]
+#
+#             # Agent picks a fractal like in 1st stage
+#             prob_frac2 = softmax_Q2p(fractals2, Data.loc[a_t, Q2_cols], beta2)
+#             frac2 = np.random.choice(fractals2, p=prob_frac2)
+#
+#             # Agent receives reward and updates 1st- and 2nd-stage values
+#             reward = np.random.choice([0, 1], p=[1 - fractal_rewards[frac2], fractal_rewards[frac2]])
+#
+#             # Model-free
+#             Q2_new = MF_update_Q2(frac2, Data.loc[a_t, Q2_cols], reward, alpha2)   # Q2 = Q2 + alpha * (reward - Q2)
+#             Q1_new = MF_update_Q1(frac1, Data.loc[a_t, Q1_cols], reward, alpha1,
+#                                   lambd, Q2_new[frac2])   # Q1 = Q1 + alpha * (Q2 - Q1 + lambd * (reward - Q1))
+#
+#             # Save trial data
+#             Data.set_value(a_t + 1, Q1_cols, Q1_new.copy())
+#             Data.set_value(a_t + 1, Q2_cols, Q2_new.copy())
+#             Data.set_value(a_t, 'frac1', frac1.copy())
+#             Data.set_value(a_t, 'frac2', frac2.copy())
+#             Data.set_value(a_t, 'reward', reward.copy())
+#
+#     return Data
+
+
+def simulate_task(mode, Data=pd.DataFrame(), par=[], n_agents=50, fractal_rewards=[0, .3, .7, 1], n_trials=100, common=0.8):
+
+    Q1_cols = [''.join(['Q1_', str(i)]) for i in range(2)]
+    Q2_cols = [''.join(['Q2_', str(i)]) for i in range(4)]
 
     # Set up big DataFrame that will hold all the simulated agents' data
-    colnames = ['AgentID', 'Q1_0', 'Q1_1', 'Q2_0', 'Q2_1', 'Q2_2', 'Q2_3', 'frac1', 'frac2', 'reward']
-    agent_length = n_trials + 1
-    Data = pd.DataFrame(columns=colnames, index=range(n_agents * agent_length))
+    if mode == 'simulate':
+        colnames = ['AgentID', 'TrialID', 'Q1_0', 'Q1_1', 'Q2_0', 'Q2_1', 'Q2_2', 'Q2_3', 'frac1', 'frac2', 'reward']
+        Data = pd.DataFrame(columns=colnames, index=range(n_agents * n_trials))
+    elif mode == 'fit':
+        n_trials = max(Data['TrialID']) + 1
+        n_agents = max(Data['AgentID']) + 1
+        fit_Q1_cols = [''.join(['fit_Q1_', str(i)]) for i in range(2)]
+        fit_Q2_cols = [''.join(['fit_Q2_', str(i)]) for i in range(4)]
+        for col in fit_Q1_cols + fit_Q2_cols:
+            Data[col] = 'NaN'
 
     # Loop through agents and generate data for each one
     for agent in range(n_agents):
 
-        # Initialize DataFrame for agent's behavior
-        agent_rows = agent * agent_length + np.array(range(agent_length))
-        Data.set_value(agent_rows, 'AgentID', agent)   # initial 1st-stage fractals
-        # Agent = pd.DataFrame(columns=colnames, index=range(n_trials + 1))
-        # Agent['AgentID'] = agent   # initial 1st-stage fractals
+        agent_rows = agent * n_trials + np.array(range(n_trials))
+        if mode == 'simulate':
+            # Initialize DataFrame for agent's behavior
+            Data.set_value(agent_rows, 'AgentID', agent)   # initial 1st-stage fractals
+            # Initial values for fractals
+            Data.set_value(agent_rows[0], Q1_cols, .5 * np.ones(2))   # initial 1st-stage fractals
+            Data.set_value(agent_rows[0], Q2_cols, .5 * np.ones(4))   # initial 2nd-stage fractals
+        elif mode == 'fit':
+            # Initial fitted values for fractals
+            Data.set_value(agent_rows[0], fit_Q1_cols, .5 * np.ones(2))   # initial 1st-stage fractals
+            Data.set_value(agent_rows[0], fit_Q2_cols, .5 * np.ones(4))   # initial 2nd-stage fractals
 
-        # Initial values for fractals
-        Q1_cols = [''.join(['Q1_', str(i)]) for i in range(2)]
-        Q2_cols = [''.join(['Q2_', str(i)]) for i in range(4)]
-        Data.set_value(agent_rows[0], Q1_cols, .5 * np.ones(2))   # initial 1st-stage fractals
-        Data.set_value(agent_rows[0], Q2_cols, .5 * np.ones(4))   # initial 2nd-stage fractals
-        # Agent.set_value(0, 'Q1', .5 * np.ones(2))   # initial 1st-stage fractals
-        # Agent.set_value(0, 'Q2', .5 * np.ones(4))   # initial 2nd-stage fractals
+        # Get agent's parameters
+        if (mode == 'simulate') and len(par) == 0:
+            par = np.random.rand(6)
 
-        # Individual behavioral parameters
-        par = np.random.rand(6)
+        # Name the parameters
         alpha1 = par[0] / 2
         alpha2 = par[1] / 2
         beta1 = par[2] * 10
@@ -86,37 +172,57 @@ def simulate_task(n_agents, fractal_rewards=[0, .3, .7, 1], n_trials=100, common
             # Get the right row in Data for this agent in this trial
             a_t = agent_rows[0] + t
 
-            # STAGE 1
-            # Display fractals
+            # Display stage-1 fractals
             fractals1 = [0, 1]
 
             # Agent's probability of choosing each fractal, gives their values
             prob_frac1 = softmax_Q2p(fractals1, Data.loc[a_t, Q1_cols], beta1)   # 1 / (1 + e ** (beta * (Qa - Qb))
 
-            # Agent picks one of the two fractals based on their probability
-            frac1 = np.random.choice(fractals1, p=prob_frac1)
+            # Agent picks one of the two fractals based on their probability and stage-2 fractals are shown
+            if mode == 'simulate':
+                # Agent picks one fractal
+                frac1 = np.random.choice(fractals1, p=prob_frac1)
+                # Stage-2 fractals are shown
+                fractals2 = select_stage2_fractals(frac1)   # frac1 == 0 => usually [0, 1]; frac1 == 1 => usually [2, 3]
+            elif mode == 'fit':
+                # Look up which fractal was picked
+                frac1 = Data.loc[a_t, 'frac1']
+                # Check which stage-2 fractal pair was shown
+                if Data.loc[a_t, 'frac2'] in [0, 1]:
+                    fractals2 = [0, 1]
+                else:
+                    fractals2 = [2, 3]
 
-            # STAGE 2
-            # Display fractals
-            fractals2 = select_stage2_fractals(frac1)   # frac1 == 0 => usually [0, 1]; frac1 == 1 => usually [2, 3]
-
-            # Agent picks a fractal like in 1st stage
+            # Agent picks a stage-2 fractal (same as in stage 1) and receives a reward
             prob_frac2 = softmax_Q2p(fractals2, Data.loc[a_t, Q2_cols], beta2)
-            frac2 = np.random.choice(fractals2, p=prob_frac2)
+            if mode == 'simulate':
+                # Agent picks a fractal
+                frac2 = np.random.choice(fractals2, p=prob_frac2)
+                # Agent potentially receives a reward, depending on reward probabilities of chosen fractal
+                reward = np.random.choice([0, 1], p=[1 - fractal_rewards[frac2], fractal_rewards[frac2]])
+            elif mode == 'fit':
+                # Look up which fractal the agent picked
+                frac2 = Data.loc[a_t, 'frac2']
+                # Check if agent got a reward
+                reward = Data.loc[a_t, 'reward']
 
-            # Agent receives reward and updates 1st- and 2nd-stage values
-            reward = np.random.choice([0, 1], p=[1 - fractal_rewards[frac2], fractal_rewards[frac2]])
-
-            # Model-free
+            # Agent updates 1st- and 2nd-stage values
             Q2_new = MF_update_Q2(frac2, Data.loc[a_t, Q2_cols], reward, alpha2)   # Q2 = Q2 + alpha * (reward - Q2)
             Q1_new = MF_update_Q1(frac1, Data.loc[a_t, Q1_cols], reward, alpha1,
                                   lambd, Q2_new[frac2])   # Q1 = Q1 + alpha * (Q2 - Q1 + lambd * (reward - Q1))
 
             # Save trial data
-            Data.set_value(a_t + 1, Q1_cols, Q1_new.copy())
-            Data.set_value(a_t + 1, Q2_cols, Q2_new.copy())
-            Data.set_value(a_t, 'frac1', frac1.copy())
-            Data.set_value(a_t, 'frac2', frac2.copy())
-            Data.set_value(a_t, 'reward', reward.copy())
+            if mode == 'simulate':
+                Data.set_value(a_t, 'frac1', frac1.copy())
+                Data.set_value(a_t, 'frac2', frac2.copy())
+                Data.set_value(a_t, 'reward', reward.copy())
+                Data.set_value(a_t, 'TrialID', t)
+                if t < (n_trials - 1):
+                    Data.set_value(a_t + 1, Q1_cols, Q1_new.copy())
+                    Data.set_value(a_t + 1, Q2_cols, Q2_new.copy())
+            elif mode == 'fit':
+                if t < (n_trials - 1):
+                    Data.set_value(a_t + 1, fit_Q1_cols, list(Q1_new.copy()))
+                    Data.set_value(a_t + 1, fit_Q2_cols, list(Q2_new.copy()))
 
     return Data
--- simulate_task.py
+++ simulate_task.py
@@ -7,24 +7,39 @@ import matplotlib.pyplot as plt
 
 
 # Simulate some data
-n_trials = 100
-Data = fun.simulate_task(n_agents=2, fractal_rewards=[0, .3, .7, 1], n_trials=n_trials, common=0.8)
-Q1_cols = [''.join(['Q1_', str(i)]) for i in range(2)]
-Q2_cols = [''.join(['Q2_', str(i)]) for i in range(4)]
-
-# Look at one agent
-Agent = Data.loc[Data['AgentID'] == 0, :]
-fig1 = plt.figure()
-for value in Agent[Q1_cols]:
-    y = np.array(Agent[value])
-    ax1 = fig1.add_subplot(121)
-    ax1.plot(range(len(y)), y)
-    ax1.set_title('Stage 1 fractal values')
-for value in Agent[Q2_cols]:
-    y = np.array(Agent[value])
-    ax2 = fig1.add_subplot(122)
-    ax2.plot(range(len(y)), y)
-    ax2.set_title('Stage 2 fractal values')
+n_trials = 150
+sim_par = [.02, .02, .4, .4, .3, 1]
+Data = fun.simulate_task(mode='simulate', n_agents=5, par=sim_par, fractal_rewards=[0, .3, .7, 1], n_trials=n_trials, common=0.8)
 
 # Fit the model to the data
-for t in range(n_trials):
+fit_par = sim_par
+fun.simulate_task(mode='fit', Data=Data, par=fit_par)
+
+# Look at one agent
+Q1_cols = [''.join(['Q1_', str(i)]) for i in range(2)]
+Q2_cols = [''.join(['Q2_', str(i)]) for i in range(4)]
+fit_Q1_cols = [''.join(['fit_Q1_', str(i)]) for i in range(2)]
+fit_Q2_cols = [''.join(['fit_Q2_', str(i)]) for i in range(4)]
+Agent = Data.loc[Data['AgentID'] == 1, :]
+fig1 = plt.figure()
+fig1.suptitle(''.join(['True par: ', str(sim_par), '; Fit par:', str(fit_par)]))
+for value in Agent[Q1_cols]:
+    y = np.array(Agent[value])
+    ax1 = fig1.add_subplot(221)
+    ax1.plot(range(len(y)), y)
+    ax1.set_title('Stage 1 true values')
+for value in Agent[Q2_cols]:
+    y = np.array(Agent[value])
+    ax2 = fig1.add_subplot(222)
+    ax2.plot(range(len(y)), y)
+    ax2.set_title('Stage 2 true values')
+for value in Agent[fit_Q1_cols]:
+    y = np.array(Agent[value])
+    ax1 = fig1.add_subplot(223)
+    ax1.plot(range(len(y)), y)
+    ax1.set_title('Stage 1 fit values')
+for value in Agent[fit_Q2_cols]:
+    y = np.array(Agent[value])
+    ax2 = fig1.add_subplot(224)
+    ax2.plot(range(len(y)), y)
+    ax2.set_title('Stage 2 fit values')
