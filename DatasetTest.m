eaafcdbc33f7813814fcc9f1ce072f199c760624
Maria Eckstein <maerry.eckstein@gmail.com>
2017-02-19 18:30:22 -0800

    test data simulation and fitting explicitly

--- DatasetTest.m
+++ DatasetTest.m
@@ -35,6 +35,113 @@ classdef DatasetTest < matlab.unittest.TestCase
             all_zeros = all(Agent(1,:) == 0);
             testCase.verifyFalse(all_zeros);
         end
+        
+        function testsimulate_task(testCase)
+            common = 0.7;
+            n_agents = 10;
+            n_trials = 200;
+            data_columns
+            
+            % Simulate agents whose decision only depend on key repetition
+            % and choice repetition, NOT on action values
+            par = [0.5 0.5 0 0 0 0.5 1 1];   % k_par and p_par are 1; beta = 0; -> only key repetition and choice repetition have an influence, not values!
+            Data = simulate_task(n_agents, n_trials, par, common);
+            Data = Data(1:end-1,:);
+            
+            fractals1_12 = Data(:,frac1_p(1)) == 1;
+            fractals1_21 = Data(:,frac1_p(1)) == 2;
+            key1_left = Data(:,key1_c) == 1;
+            prev_key1_left = [0; key1_left(1:end-1)];
+            prev_key1_right = 1 - prev_key1_left;
+            frac1_1 = Data(:,frac1_c) == 1;
+            prev_frac1_1 = [0; frac1_1(1:end-1)];
+            prev_frac1_2 = 1 - prev_frac1_1;
+            
+            % Look at trials, in which agents should choose left
+            choose_left = (fractals1_12 & prev_key1_left & prev_frac1_1) | ...
+                          (fractals1_21 & prev_key1_left & prev_frac1_2);
+            
+            testCase.verifyTrue(round(mean(Data(choose_left(2:end), key1_c))) == 1);
+
+            % Agents should choose right
+            choose_right = (fractals1_12 & prev_key1_right & prev_frac1_2) | ...
+                           (fractals1_21 & prev_key1_right & prev_frac1_1);
+            
+            testCase.verifyTrue(round(mean(Data(choose_right(2:end),key1_c))) == 2);
+
+            choose_frac1 = (fractals1_12 & prev_key1_left & prev_frac1_1) | ...
+                           (fractals1_21 & prev_key1_right & prev_frac1_1);
+
+            testCase.verifyTrue(round(mean(Data(choose_frac1(2:end),frac1_c))) == 1);
+
+            choose_frac2 = (fractals1_12 & prev_key1_right & prev_frac1_2) | ...
+                           (fractals1_21 & prev_key1_left & prev_frac1_2);
+
+            testCase.verifyTrue(round(mean(Data(choose_frac2(2:end), frac1_c))) == 2);
+
+            % Key repetition and choice repetition balance each other
+            undetermined = (fractals1_12 & prev_key1_right & prev_frac1_1) | ...
+                           (fractals1_21 & prev_key1_right & prev_frac1_2) | ...
+                           (fractals1_12 & prev_key1_left & prev_frac1_2) | ...
+                           (fractals1_21 & prev_key1_left & prev_frac1_1);
+            
+            testCase.verifyTrue(mean(Data(undetermined(2:end),key1_c)) > 1.3);
+            testCase.verifyTrue(mean(Data(undetermined(2:end),frac1_c)) > 1.3);
+            
+            % Compute BIC & AIC for these perfect agents
+            n_fit = n_trials;
+            output = 'all';
+            data_type = 'sim';
+            NLLBICAICData = computeNLL(Data, par, n_fit, output, common, data_type);
+            BICData = NLLBICAICData(2);
+            
+            %%% To test computeNLL, I could run computeNLL on the agents
+            %%% just created and on agents with random behavior (just a
+            %%% data frame with random 1's and 2's) and check if the BIC
+            %%% and AIC are smaller for the created than randomized agents
+            
+            randData = zeros(n_agents * n_trials, 40);
+            randData(:, frac1_c) = randsample([1 2], n_agents * n_trials, true);
+            randData(:, frac1_p(1)) = randsample([1 2], n_agents * n_trials, true);
+            randData(:, frac1_p(2)) = -(randData(:, frac1_p(1)) - 3);
+            randData(:, key1_c) = randsample([1 2], n_agents * n_trials, true);
+            randData(:, key2_c) = randsample([1 2], n_agents * n_trials, true);
+            randData(:, frac2_c) = randsample(1:4, n_agents * n_trials, true);
+            randData(:, frac2_p(1)) = randsample(1:4, n_agents * n_trials, true);
+            fill = randData(:, frac2_p(1)) - 1;
+            fill(fill == 2) = 4;
+            fill(fill == 0) = 2;
+            randData(:, frac2_p(2)) = fill;
+            
+            n_fit = n_trials;
+            output = 'all';
+            data_type = 'sim';
+            NLLBICAICRand = computeNLL(randData, par, n_fit, output, common, data_type);
+            BICRand = NLLBICAICRand(2);
+            
+            testCase.verifyTrue(BICData < BICRand);
+            
+            
+            % Simulate agents whose decision only depend on mf VALUES
+            par = [0.5 0.5 1 1 0 0 0.5 0.5];   % beta = 1; k_par and p_par = 0 -> only values influence choice!
+            mfData = simulate_task(n_agents, n_trials, par, common);
+            mfData = mfData(1:end-1,:);
+            
+            frac1_1_high = mfData(:,Qmf1_c(1)) > (mfData(:,Qmf1_c(2)) + 0.3);
+            frac1_2_high = mfData(:,Qmf1_c(2)) > (mfData(:,Qmf1_c(1)) + 0.3);
+            frac1_12_similar = ~(frac1_1_high | frac1_2_high);
+            
+            % Look at trials, in which agents should choose left
+            testCase.verifyTrue(round(mean(mfData(frac1_1_high, frac1_c))) == 1);   % frac1 is selected most of the time, frac2 barely
+            
+            % Look at trials, in which agents should choose right
+            testCase.verifyTrue(round(mean(mfData(frac1_2_high, frac1_c))) == 2);   % frac2 is selected most of the time, frac1 barely
+            
+            % Look at trials, in which agents should be uncertain
+            fracs_chosen = mean(mfData(frac1_12_similar, frac1_c));
+            testCase.verifyTrue(fracs_chosen > 1.4 & fracs_chosen < 1.6);   % frac2 and frac1 are selected equally often
+            
+        end
                 
         function testRealData2015(testCase)
             data_year = 2015;
